47162a17d3e803e8af2b8abd2db0c9dbe82436a5
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-06 23:36:48 +0530

    number-guessing-game

---
+++ basics/number-guessing-game/main.py
@@ -0,0 +1,42 @@
+import random
+
+random_number = random.randint(1,100)
+
+def too_high():
+  print("too high!")
+
+def too_low():
+  print("too low!")
+
+def congo():
+   print("YOU GOT IT !!!!!")
+
+def user_guess():
+  while True:
+    try:
+      a = int(input("enter a number between 1 and 100:"))
+      if 1 <= a <= 100:
+        return a
+      else:
+        print("number must be between 1 and 100")
+    except ValueError:
+      print("Invalid Input !!!!! Please enter a whole number")
+
+   
+def main():
+  while True:
+    
+    guess = user_guess()
+    if random_number == guess:
+      congo()
+      break
+
+    elif random_number > guess:
+      too_low()
+    
+    elif random_number < guess:
+      too_high()
+          
+if __name__ == "__main__" :
+  main() 
+   
---
+++ basics/number-guessing-game/readme.md
@@ -0,0 +1,17 @@
+# Number Guessing Game
+
+This is a small Python project I made while practicing basic concepts.
+
+The program generates a random number between 1 and 100 and the user has to guess it.  
+After each guess, it tells whether the number is too high or too low until the correct number is guessed.
+
+## Concepts used
+- random module
+- functions
+- while loop
+- if-else conditions
+- try and except for invalid input
+
+## How to play
+Run the file and enter a number between 1 and 100 when asked.  
+Keep guessing until you get the correct number
